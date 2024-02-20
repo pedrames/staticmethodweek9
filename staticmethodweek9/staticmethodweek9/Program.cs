ed446fb1d11359f84ae9fe47a92fa476dd06a883
pedrames <91216198+pedrames@users.noreply.github.com>
2024-02-20 00:35:09 -0800

    1

---
+++ staticmethodweek9/staticmethodweek9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace staticmethodweek9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
